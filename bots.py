a23a0936aa1178397e3050b8eb5e95bef2cc114f
Luis Moreno <pipemoreno9405@gmail.com>
2021-12-14 17:16:19 -0500

    Change to one request per page for scrapping journals

--- bots.py
+++ bots.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 from requests.models import HTTPError
+from requests.exceptions import Timeout
 
 
 BASE_URL = "https://revistas.uexternado.edu.co/index.php/"
@@ -18,66 +19,118 @@ class AuditBot():
 
     def valid_URL(self, url: str) -> bool:
         try:
-            requests.get(url=url).raise_for_status()
-        except HTTPError:
+            requests.get(url=url, timeout=3).raise_for_status()
+        except (HTTPError, Timeout):
             return False
         else:
             return True
 
-    def inspect_sidebar_blocks(self, url: str):
-        soup = BeautifulSoup(requests.get(
-            url=url, headers=headers).text, "html.parser")
-        block_names = [block.text.lower() for block in soup.select(
-            selector=".title")]
-        custom_blocks = {
-            'journal': soup.title.text.strip('\n\t'),
-            'block_names': block_names
-        }
-        return custom_blocks
+    def inspect_page_components(self, soup: BeautifulSoup) -> dict:
 
-    def inspect_journal_home(self, url: str):
-        soup = BeautifulSoup(requests.get(
-            url=url, headers=headers).text, "html.parser")
+        checklist = {
+            'titulo articulo': '.media-heading a',
+            'autores': '.authors',
+            'vistas_pdf': '.issue-stats.text-muted',
+            'galerada_pdf': '.galley-link.btn.obj_galley_link.pdf',
+            'logo_revista': '.navbar-brand-logo.logo_revista'
+        }
+
+        components = {name: 'SI' for name, selector in checklist.items(
+        ) if soup.select_one(selector=selector) != None}
+
+        # components['botones_metricas'] = {a.text.lower(): a.get(
+        #     'href') for a in soup.select(selector='.btn.btn-default.analytics_btn') if self.valid_URL(a.get('href'))}
+        components['metric_buttons'] = {a.text.lower(): a.get(
+            'href') for a in soup.select(selector='.btn.btn-default.analytics_btn')}
+        return components
+
+    def inspect_navigation_bar(self, soup: BeautifulSoup) -> dict:
         nav_bar_items = [
             item.text.strip('\n\t').lower() for item in soup.select('#main-navigation li')]
         nav_bar_URLs = [a.get('href') for a in soup.select(
             selector="#main-navigation li a")]
 
         nav_bar = {}
+
         for i in range(len(nav_bar_items)):
             if self.valid_URL(nav_bar_URLs[i]):
                 nav_bar[nav_bar_items[i]] = nav_bar_URLs[i]
+
         return nav_bar
 
-    def extract_urls(self):
+    def inspect_sidebar_blocks(self, soup: BeautifulSoup) -> dict:
+
+        block_names = [block.text.lower() for block in soup.select(
+            selector=".title")]
+        custom_blocks = {
+            'block_names': block_names
+        }
+        return custom_blocks
+
+    def inspect_journal_home(self, url: str) -> dict:
+        try:
+            request = requests.get(url=url, headers=headers, timeout=10)
+        except Timeout:
+            return {}
+        else:
+            soup = BeautifulSoup(request.text, "html.parser")
+
+            checklist = {
+                'journal': soup.title.text.strip('\n\t')
+            }
+
+            nav_bar = self.inspect_navigation_bar(soup)
+            blocks = self.inspect_sidebar_blocks(soup)
+            components = self.inspect_page_components(soup)
+
+            checklist.update(nav_bar)
+            checklist.update(blocks)
+            checklist.update(components)
+
+            return checklist
+
+    def extract_urls(self) -> list:
         sitemap_url = f"{BASE_URL}index/sitemap"
         soup = BeautifulSoup(requests.get(
             url=sitemap_url, headers=headers).text, "lxml-xml")
         urls = [tag.text.strip('sitemap') for tag in soup.find_all(name="loc")]
         return urls
 
-    def audit_journals(self):
+    def audit_journals(self) -> None:
         self.create_report()
         for journal_url in self.extract_urls():
-            blocks = self.inspect_sidebar_blocks(url=journal_url)
-            nav_bar = self.inspect_journal_home(url=journal_url)
-            self.fill_report(blocks=blocks, nav_bar=nav_bar)
+            # blocks = self.inspect_sidebar_blocks(url=journal_url)
+            # nav_bar = self.inspect_journal_home(url=journal_url)
+            checklist = self.inspect_journal_home(url=journal_url)
+            self.fill_report(checklist=checklist)
 
-    def create_report(self):
+    def create_report(self) -> None:
         with open('report.csv', mode='w') as file:
             file.write(
-                'Revista,Tutoriales,Acerca de,Formatos,Indexada en,Scholar,mas_leidos,Palabras clave,Más descargados,Código QR,Información,Inicio revista,Avisos,Actual,Archivos\n')
+                'Revista,Tutoriales,Acerca de,Formatos,Indexada en,Scholar,mas_leidos,Palabras clave,Más descargados,\
+                Código QR,Información,Inicio revista,Avisos,Actual,Archivos,titulo articulo,autores,vistas pdf,\
+                galerada pdf,logo revista,métricas,estadísticas de la revista,analíticas\n')
 
-    def fill_report(self, blocks: dict, nav_bar: dict):
-        check_list = ['tutoriales', 'acerca de', 'formatos',
-                      'indexada en', 'scholar', 'mas_leidos', 'palabras clave', 'más descargados en los ultimos 30 días', 'código qr', 'información', 'inicio revista', 'avisos', 'actual', 'archivos']
+    def fill_report(self, checklist: dict) -> None:
+        columns = ['tutoriales', 'acerca de', 'formatos',
+                   'indexada en', 'scholar', 'mas_leidos', 'palabras clave', 'más descargados en los ultimos 30 días',
+                   'código qr', 'información', 'inicio revista', 'avisos', 'actual', 'archivos', 'titulo articulo',
+                   'autores', 'vistas_pdf', 'galerada_pdf', 'logo_revista', 'métricas', 'estadísticas de la revista', 'analíticas']
         with open(file='report.csv', mode='a') as file:
-            file.write(f"{blocks.get('journal')},")
-            for item in check_list:
-                if item in blocks.get('block_names'):
+            file.write(f"{checklist.get('journal')},")
+            for column in columns:
+                if column in checklist.keys():
                     file.write('SI,')
-                elif item in nav_bar.keys():
-                    file.write('SI,')
+                elif checklist.get('block_names') != None:
+                    if column in checklist['block_names']:
+                        file.write('SI,')
+                    else:
+                        file.write('NO,')
+                elif checklist.get('metric_buttons') != None:
+                    if column in checklist['metric_buttons'].keys():
+                        file.write('SI,')
+                    else:
+                        file.write('NO,')
                 else:
                     file.write('NO,')
 
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+from os import name
 from bots import AuditBot
 from mail_service import send_mail
 
-#COMPLETED 1: Make a class for audit bot
-#COMPLETED 2: Extract URL list from sitemap
-#COMPLETED 3: Create a mail service for sending reports
+# COMPLETED 1: Make a class for audit bot
+# COMPLETED 2: Extract URL list from sitemap
+# COMPLETED 3: Create a function for checking sidebar blocks (custom and PKP)
+# COMPLETED 4: Create a funtion for checking navigation bar items and URLs
+# COMPLETED 5: Create a function for URL validation
+# COMPLETED 6: Create a function for filling reports to send by email
+# COMPLETED 7: Create a mail service for sending reports
+# IN PROCESS 10: Create a function to get current issue for audit components
 
 
 bot = AuditBot()
 
-send_mail()
+# print(bot.inspect_journal_home('https://revistas.uexternado.edu.co/index.php/contad/'))
 
 
 
+# send_mail()
